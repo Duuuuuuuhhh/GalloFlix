ac24edc8072a850134b9f302d41c6e727c2e71a9
Eduardo Simionato Nogueira <edunogueira793@gmail.com>
2023-05-16 08:34:02 -0300

    criação do Movie Genre

--- GalloFlix/Models/Movie.cs
+++ GalloFlix/Models/Movie.cs
@@ -1,12 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace GalloFlix.Models
+namespace GalloFlix.Models;
+
+public class Movie
 {
-    public class Movie
-    {
+        [Key]
+        public int Id {get; set;}
+
+        [Required]
+        [StringLength(100)]
+        public string Title {get; set;}
+
+        [Required]
+        [StringLength(100)]
+        public string OriginalTitle {get; set;}
+
+        [Required]
+        [StringLength(8000)]
+        public string Synopsis {get; set;}
+
+        [Column(TypeName = "Year")]
+        [Required]
+        public Int16 MovieYear { get; set; }
+
+        [Required]
+        public Int16 Duration { get; set; }
         
-    }
+        [Required]
+        public byte AgeRating {get; set;} = 0;
+
+        [StringLength(200)]
+        public string Image {get; set;}
+
+        [NotMapped]
+        public string HourDuration {get {
+            return TimeSpan.FromMinutes(Duration).ToString(@"%h' h 'mm' min'"  );
+        }}
+    
 }
